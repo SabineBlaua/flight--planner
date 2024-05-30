e4aaaaceb7c1863354206aafa2dbe60595693df6
SabineBlaua <sabineblaua@gmail.com>
2024-05-30 10:00:37 +0300

    Initial commit

---
+++ src/test/java/io/codelex/flight_planner/FlightPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codelex.flight_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
